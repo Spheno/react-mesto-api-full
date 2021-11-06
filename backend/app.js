a8e0f397bec5edbea2d1eb72e80359b35856503d
Valeriia Dubrovskaia <sphen0@yandex.ru>
2021-11-06 22:42:46 +0500

    crash test

--- backend/app.js
+++ backend/app.js
@@ -43,6 +43,12 @@ const options = {
 
 app.use('*', cors(options));
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
